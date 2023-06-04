276527c39d0ddc69d4abe9052bee1470b084a2e9
TiuAn <adwithwosso@gmail.com>
2023-06-04 16:56:25 +0700

    Connect mongoDB

--- server/index.js
+++ server/index.js
@@ -1,4 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+	try {
+		await mongoose.connect(
+			`mongodb+srv://hoang-son-mern-app:abcde12345@mern-app.pwyryrw.mongodb.net/?retryWrites=true&w=majority`,
+			{
+				useNewUrlParser: true,
+				useUnifiedTopology: true,
+			},
+		);
+		console.log('Connected MongoDB');
+	} catch (error) {
+		console.log(error.message);
+		process.exit(1);
+	}
+};
+
+connectDB();
 
 const app = express();
 
