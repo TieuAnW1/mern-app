1d38c672f997aad4869682160bf7a98687cfd6b1
TiuAn <adwithwosso@gmail.com>
2023-06-09 13:46:41 +0700

    Api get all posts of user

--- server/routes/post.js
+++ server/routes/post.js
@@ -3,7 +3,19 @@ const router = express.Router();
 const verifyToken = require('../middleware/auth');
 
 const Post = require('../models/post');
+const { route } = require('./auth');
 
+/*
+	@route POST api/posts/create
+	@desc Create new post
+	@access Private
+	@param {
+		title: String,
+		description: String,
+		url: String,
+		status: String,
+	}
+*/
 router.post('/create', verifyToken, async (req, res) => {
 	const { title, description, url, status } = req.body;
 	if (!title)
@@ -18,7 +30,7 @@ router.post('/create', verifyToken, async (req, res) => {
 			description,
 			url,
 			status: status || 'TO LEARN',
-			user: '647cbcc0996c2c57b8bcfe40',
+			user: req.userId,
 		});
 
 		await newPost.save();
@@ -27,6 +39,29 @@ router.post('/create', verifyToken, async (req, res) => {
 			success: true,
 			message: 'Post was created successfully.',
 			post: newPost,
+		});
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({
+			success: false,
+			message: 'Internal server error.',
+		});
+	}
+});
+
+/*
+	@route POST api/posts/get-posts
+	@desc Get all posts of user
+	@access Private
+*/
+
+router.get('/get-posts', verifyToken, async (req, res) => {
+	try {
+		const posts = await Post.find({ user: req.userId }).populate('user', ['userName']);
+		return res.json({
+			success: true,
+			message: 'Getting all posts was successfully.',
+			posts: posts,
 		});
 	} catch (error) {
 		console.log(error);
